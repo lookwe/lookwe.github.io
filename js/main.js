51c3ee10a13eeb01555baa14cb5942cbd27daa9b
黄钟飞 <2726768601@qq.com>
2020-04-03 04:56:06 +0800

    冲突

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 setInterval(() => {
    let h2 = document.querySelectorAll('.content section div h2');
-
    for (let i = 0; i<h2.length;i++) {
      h2[i].innerHTML = new Date().toLocaleString();
    }
